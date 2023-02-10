c23d916e1c695ac27b006b54ba256e8aabc8ac7f
Gungun Gupta <gungungupta1404@gmail.com>
2023-02-10 22:26:00 +0530

    Update sendmsg.py

--- sendmsg.py
+++ sendmsg.py
@@ -1,5 +1,5 @@
 from twilio.rest import Client
-sid='ACe30a0cc15a4ecb75e59013d1ab25211f'
-auth='dcc216cc5b4b42f09449f3816697f7c0'
+sid='SID_DAALO_APNI'
+auth='PASSWORD_DAALDO'
 client=Client(sid, auth)
-message=client.messages.create(to='whatsapp:+919310329696', from_ ='whatsapp:+14155238886',body='Hi, this is the test message')
+message=client.messages.create(to='whatsapp:+APNA_NUMBER_DAALO', from_ ='whatsapp:+14155238886',body='Hi, this is the test message')
