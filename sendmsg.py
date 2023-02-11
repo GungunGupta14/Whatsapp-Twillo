9cf43373488e93b19c7289a541ecc54151db51ed
Gungun Gupta <104621253+GungunGupta14@users.noreply.github.com>
2023-02-11 15:10:17 +0530

    Update sendmsg.py

--- sendmsg.py
+++ sendmsg.py
@@ -1,7 +1,7 @@
 from twilio.rest import Client
 def message(number,message):
-    sid='ACe30a0cc15a4ecb75e59013d1ab25211f'
-    authToken='e7730d793b1d95a66010fdf6eb444917'
+    sid='PUT_SiD_no.'
+    authToken='put_password'
     client=Client(sid,authToken)
     
     message=client.messages.create(to='whatsapp:'+number,
